65a8061698f7f6f1726708647ad6792fb6a46f07
Dave Lockhart <dave.lockhart@d2l.com>
2014-10-31 16:36:24 -0400

    Only joining paths for relative paths

--- test/loadDefinitionSpec.js
+++ test/loadDefinitionSpec.js
@@ -7,12 +7,21 @@ var dataPath = path.join( __dirname, 'data' );
 
 describe( 'Load Definition', function() {
 
-	it( 'Should load 2 files', function( done ) {
+	it( 'Should resolve relative paths', function( done ) {
 		loadDefinition( path.join( dataPath, 'packages', 'definition1.definition.xml' ) )
 			.then( function( files ) {
 				expect( files.length ).toBe( 2 );
 				expect( files[0] ).toBe( path.join( dataPath, 'test1.package.xml' ) );
 				expect( files[1] ).toBe( path.join( dataPath, 'test2.package.xml' ) );
+				done();
+			} );
+	} );
+
+	it( 'Should leave absolute paths alone', function( done ) {
+		loadDefinition( path.join( dataPath, 'packages', 'definition2.definition.xml' ) )
+			.then( function( files ) {
+				expect( files.length ).toBe( 1 );
+				expect( files[0] ).toBe( 'c:\\test1.package.xml' );
 				done();
 			} );
 	} );
