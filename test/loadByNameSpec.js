3e16dab4f9857562ad12be7aa546fa5cc57a37eb
Dave Lockhart <dave.lockhart@d2l.com>
2014-11-04 15:45:13 -0500

    removing support for environment variable

--- test/loadByNameSpec.js
+++ test/loadByNameSpec.js
@@ -8,14 +8,6 @@ var dataPath = path.join( __dirname, 'data' );
 describe( 'Load By Name', function() {
 
 	var opts = { webPackagePath: path.join( dataPath, 'packages' ) };
-	var oldEnv;
-
-	beforeEach( function() {
-		oldEnv = process.env.WEB_PACKAGE_PATH;
-	} );
-	afterEach( function() {
-		process.env.WEB_PACKAGE_PATH = oldEnv;
-	} );
 
 	it( 'should load 4 files across 2 package files', function( done ) {
 		webPackageReader.loadByName( 'definition1', opts )
@@ -29,21 +21,7 @@ describe( 'Load By Name', function() {
 			} );
 	} );
 
-	it( 'should default to WEB_PACKAGE_PATH env variable if option is missing', function( done ) {
-
-		process.env.WEB_PACKAGE_PATH = opts.webPackagePath;
-
-		webPackageReader.loadByName( 'definition1' )
-			.then( function( files ) {
-				expect( files.length ).toBe( 4 );
-				done();
-			} );
-
-	} );
-
 	it( 'should throw if no web package path is defined', function( done ) {
-
-		process.env.WEB_PACKAGE_PATH = '';
 
 		webPackageReader.loadByName( 'definition1' )
 			.fail( function( err ) {
